67982cd2b7a7264d54d3f9fb06d7ac2ad3e4ad08
Maks Świerczyński <maksswierczynski@gmail.com>
2021-02-28 15:22:57 +0100

    build client

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
 // age, isAdmin, name, id
 const express = require('express')
 const app = express();
-const port = 3000
 const sequelize = require('./sequelize');
-const cors = require('cors');
+// const cors = require('cors');
+const path = require('path');
 
-const  whitelist = ['http://localhost:3001', 'http://localhost:3000']
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+const port = 3000
 
-app.use(cors(corsOptions))
+// const  whitelist = ['http://localhost:3001', 'http://localhost:3000']
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
+
+// app.use(cors(corsOptions))
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/api/users', async (req, res) => {
-    const users = await sequelize.models.user.findAll();
-    res.send(users);
+  const users = await sequelize.models.user.findAll();
+  console.log(__dirname);
+  res.send(users);
 })
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
